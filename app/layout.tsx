163eb3aa3bfadea8338f5ca9b92de7cf043c871f
alvinaleguiojo <alvin.aleguiojo@gmail.com>
2023-01-01 12:45:56 +0800

    fixing seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
+      <head lang="en">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Project</title>
       </head>
